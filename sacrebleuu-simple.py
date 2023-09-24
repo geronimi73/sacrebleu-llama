fba566395d8b3ff1683652be73ebc3bdf2719fc9
geronimi73 <141400217+geronimi73@users.noreply.github.com>
2023-09-24 19:56:00 +0200

    Update sacrebleuu-simple.py

--- sacrebleuu-simple.py
+++ sacrebleuu-simple.py
@@ -2,13 +2,13 @@ import transformers, evaluate, torch
 from datasets import load_dataset
 from tqdm import tqdm
 
-pipeline = pipeline = transformers.pipeline("text-generation",
-    model="Llama-2-7b-hf",
+pipeline  = transformers.pipeline("text-generation",
+    model="models/llama2-7b",
     device_map="auto",
     torch_dtype=torch.bfloat16)
 
-ds = load_dataset(path="wmt20_mlqe_task1", name="en-de",split="test")
-ds=ds.select(range(12))["translation"]
+ds=load_dataset(path="wmt20_mlqe_task1", name="en-de",split="test")
+ds=ds["translation"]
 
 ds_examples=ds[0:5]
 ds_predict=ds[5:]
@@ -23,6 +23,7 @@ gen_config = {
     "top_k": 40,
     "do_sample": True,
     "max_new_tokens": 100,  
+    "pad_token_id": pipeline.tokenizer.eos_token_id,
 }
 
 predictions=[]
@@ -30,8 +31,8 @@ for row in tqdm(ds_predict):
     prompt=prompt_examples + "\n\n" + prompt_template.format(en=row["en"], de="")[:-1]
     prediction=pipeline(prompt, **gen_config)[0]["generated_text"][len(prompt)+1:]
 
-    if "\n\n" in prediction:
-        prediction=prediction.split("\n\n")[0]
+    if "\n" in prediction:
+        prediction=prediction.split("\n")[0]
     predictions.append(prediction)
 
 references=[row["de"] for row in ds_predict]
